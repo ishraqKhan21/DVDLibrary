5afde32c020a27a1a478120eef6115e679aef2dd
ishraqKhan21 <59457787+ishraqKhan21@users.noreply.github.com>
2022-04-06 17:11:30 -0400

    fixing merge

---
+++ dvdlibrary/src/main/java/com/m3/dvdlibrary/controller/DvdLibraryController.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m3.dvdlibrary.controller;
+
+import com.m3.dvdlibrary.dto.DVD;
+import com.m3.dvdlibrary.dao.DvdLibraryFileImpl;
+import com.m3.dvdlibrary.ui.DvdLibraryView;
+import com.m3.dvdlibrary.ui.UserIO;
+import com.m3.dvdlibrary.ui.UserIOConsoleImpl;
+import java.util.List;
+
+/**
+ *
+ * @author andri
+ */
+public class DvdLibraryController {
+
+    private final DvdLibraryFileImpl dao = new DvdLibraryFileImpl();
+    private final DvdLibraryView view = new DvdLibraryView();
+    private final UserIO io = new UserIOConsoleImpl();
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection;
+        while (keepGoing) {
+            menuSelection = view.printMenuAndGetSelection();
+            switch (menuSelection) {
+                case 1:
+                    createDVD();
+                    break;
+                case 2:
+                    io.print("ADD A LIST OF DVDs TO THE COLLECTION");
+                    break;
+                case 3:
+                    io.print("REMOVE ONE DVD FROM THE COLLECTION");
+                    break;
+                case 4:
+                    io.print("REMOVE A LIST OF DVDs FROM THE COLLECTION");
+                    break;
+                case 5:
+                    io.print("UPDATE/EDIT ONE DVDs INFO IN THE COLLECTION");
+                    updateTheDVD();
+                    break;
+                case 6:
+                    io.print("UPDATE/EDIT A LIST OF DVDs INFO IN THE COLLECTION");
+                    break;
+                case 7:
+                    listDVDs();
+                    break;
+                case 8:
+                    io.print("DISPLAY ONE DVD INFO");
+                    break;
+                case 9:
+                    io.print("SEARCH FOR ONE DVD BY TITLE");
+                    break;
+                case 10:
+                    io.print("LOAD DVD LIBRARY FROM THE FILE");
+                    break;
+                case 11:
+                    io.print("SAVE DVD LIBRARY BACK TO THE FILE WHEN PROGRAM COMPLETES");
+                    break;
+                case 12:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.print("UNKNOWN COMMAND");
+            }
+
+        }
+        io.print("GOOD BYE");
+    }
+
+    private void createDVD() {
+        view.displayCreateDVDBanner();
+        DVD newDVD = view.getNewDVDInfo();
+        dao.addDVD(newDVD.getTitle(), newDVD);
+        view.displayCreateSuccessBanner();
+    }
+    
+     private void listDVDs() {
+        view.displayDisplayAllBanner();
+        List<DVD> dvdList = dao.getAllDVDs();
+        view.displayDVDList(dvdList);
+    }
+     
+     private void updateTheDVD() {
+         view.displayUpdateBanner();
+         DVD updatedDVD = view.getNewDVDInfo();
+         dao.updateDVD(updatedDVD.getTitle(), updatedDVD);
+         view.displayUpdateSuccessBanner();
+     }
+     
+     
+}
