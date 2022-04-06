6a0c846f60ebbafe1728049714185647e8a14b23
ishraqKhan21 <59457787+ishraqKhan21@users.noreply.github.com>
2022-04-06 16:54:27 -0400

    MyUpdate
    just requirement 5

---
+++ DVDLibrary/src/main/java/com/m3/dvdlibrary/dao/DvdLibraryFileImpl.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.m3.dvdlibrary.dao;
+
+import com.m3.dvdlibrary.dto.DVD;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author andri
+ */
+public class DvdLibraryFileImpl implements DvdLibraryDao {
+
+// for virtual storage
+    private Map<String, DVD> DVDs = new HashMap<>();
+
+    @Override
+    public DVD addDVD(String title, DVD dvd) {
+        DVD previousDVD = DVDs.put(title, dvd); // previous Value of the key 
+        return previousDVD;
+    }
+
+    @Override
+    public List<DVD> addDVDs(String[] titles, DVD dvds) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public DVD removeDVD(String title) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<DVD> removeDVDs(String[] titles, DVD dvds) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public DVD updateDVD(String title, DVD dvd) {
+        if (!DVDs.containsKey(title)) {
+            System.out.println("Does not exist");
+            return null;
+        }
+        DVD updatedDVD = DVDs.put(title, dvd);
+        return updatedDVD;
+
+    }
+
+    @Override
+    public DVD updateDVDs(String[] titles, DVD dvds) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public DVD getDVD(String title) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<DVD> getAllDVDs() {
+        return new ArrayList<DVD>(DVDs.values());
+    }
+
+    @Override
+    public void displayDVDinfo(DVD dvd, String info) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void loadDVDs() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void saveDVDs() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
