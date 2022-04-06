05a4c95766fa0cf06a2ed8bc580038a72f789e6f
BrandonATurner <102608959+BrandonATurner@users.noreply.github.com>
2022-04-06 11:57:54 -0400

    Update DVD.java

--- dvdlibrary/src/main/java/com/m3/dvdlibrary/dto/DVD.java
+++ dvdlibrary/src/main/java/com/m3/dvdlibrary/dto/DVD.java
@@ -7,4 +7,59 @@ package com.m3.dvdlibrary.dto;
  */
 public class DVD {
 
+    private String title;
+    private String releaseDate;
+    private String mpaaRating;
+    private String directorName;
+    private String studio;
+    private String review;
+    
+    public DVD(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getMpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setMpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+
+    public String getDirectorName() {
+        return directorName;
+    }
+
+    public void setDirectorName(String directorName) {
+        this.directorName = directorName;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
 }
